5ad30aa7f4a81ee8cee30c7c421ced30d06e4130
nzkim1234 <nzkim1234@naver.com>
2022-08-30 22:12:13 +0900

    websocket 추가

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
+import http from "http";
 import express from "express";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -9,4 +11,18 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({server});
+
+wss.on("connection", (socket) => {
+    console.log('Connected to Browser ')
+    socket.on("close", () => console.log('Disconnected from the browser'));
+    socket.on('message', (message) => {
+        console.log(message.toString());
+    })
+    socket.send('hello');
+});
+
+server.listen(3000, handleListen);
